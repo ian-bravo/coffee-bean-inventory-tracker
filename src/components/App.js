c7e6ea36bc4056a463fb80e5225923c743792974
ian-bravo <bravo.ian@gmail.com>
2023-11-17 08:48:23 -0800

    add most components

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import BeanControl from "./BeanControl";
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <BeanControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
