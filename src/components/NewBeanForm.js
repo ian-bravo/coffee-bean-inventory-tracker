0c3dcae90cd97caef3371d5a1e4dbdc63b85631a
ian-bravo <bravo.ian@gmail.com>
2023-11-17 11:09:13 -0800

    refactor form

--- src/components/NewBeanForm.js
+++ src/components/NewBeanForm.js
@@ -1,9 +1,28 @@
 import React from "react";
 
 function NewBeanForm(props){
+
+  function handleNewBeanFormSubmission(event) {
+    event.preventDefault();
+  }
+
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewBeanFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Bean name' />
+        <input
+          type='text'
+          name='roast'
+          placeholder='Roast type' />
+        <input
+          type='text'
+          name='price'
+          placeholder='$/lb.' />
+        <button type='submit'>Add</button>
+      </form>
     </React.Fragment>
   );
 }
